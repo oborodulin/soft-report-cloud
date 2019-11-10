3c4940702e826b945f57594f850bcdf4d8740f6c
Oleg Borodulin <soft.oborodulin@gmail.com>
2019-11-10 20:32:33 +0300

    Разделение проекта на модули

---
+++ soft-report/src/main/java/com/oborodulin/softreport/SoftReportApplication.java
@@ -0,0 +1,13 @@
+package com.oborodulin.softreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftReportApplication.class, args);
+	}
+
+}
